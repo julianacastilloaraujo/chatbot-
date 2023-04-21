7b372d132c0e54ac2485820d1c018e1725ce6670
Juliana Castillo <jcastilloaraujo@icloud.com>
2023-04-21 00:13:54 -0600

    cambios en app.js

--- base-baileys-memory/app.js
+++ base-baileys-memory/app.js
@@ -1,15 +1,19 @@
 /** (1) SECCION IMPORTACIONES CHAT TECNM */
-
 const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
 const QRPortalWeb = require('@bot-whatsapp/portal')
+
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+
 const MockAdapter = require('@bot-whatsapp/database/mock')
 
+const conversacionInicial = addKeyword('hola').addAnswer('Hola, bienvenido al chatbot del TECNM 🇲🇽')
 /** (2) DEFINICION LA FUNCION PRINCIPAL */
 const main = async () => {
     const adapterDB = new MockAdapter()
-    const adapterFlow = createFlow([flowPrincipal])
+
     const adapterProvider = createProvider(BaileysProvider)
+
+    const adapterFlow = createFlow([conversacionInicial])
 
     createBot({
         flow: adapterFlow,
@@ -19,5 +23,6 @@ const main = async () => {
 
     QRPortalWeb()
 }
+
 
 main()
