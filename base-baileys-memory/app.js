ec04b898d82e848f34f22b61ea9cd2e271f1ec76
Juliana Castillo <jcastilloaraujo@icloud.com>
2023-04-21 00:25:40 -0600

    subida de Arrays basicos

--- base-baileys-memory/app.js
+++ base-baileys-memory/app.js
@@ -6,8 +6,9 @@ const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 
 const MockAdapter = require('@bot-whatsapp/database/mock')
 
-const conversacionInicial = addKeyword('Hola').addAnswer('Hola, bienvenido al chatbot TECNM como ayuda de prevencion hacia "Depresion y Suicidio" en el Instituto Tecnologico de Merida ubicado en el estado de Yucatán, México')
+const conversacionInicial = addKeyword('Hola', 'hola', 'buenas', 'ey', 'hey').addAnswer('Hola, bienvenido al chatbot TECNM como ayuda de prevencion hacia "Depresion y Suicidio" en el Instituto Tecnologico de Merida ubicado en el estado de Yucatán, México')
 /** conversacionInicial es el primer flow (flujo conversacional del chatbot) */
+/** conversacionInicial es el primer flow ([Arrays]) en addKeyword */
 
 /** (2) DEFINICION LA FUNCION PRINCIPAL */
 const main = async () => {
