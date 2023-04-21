43c040e0f21b4ca81c6fd9fe27dce26f1b9210a1
Juliana Castillo <jcastilloaraujo@icloud.com>
2023-04-21 00:18:37 -0600

    cambios en app.js

--- base-baileys-memory/app.js
+++ base-baileys-memory/app.js
@@ -6,7 +6,7 @@ const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 
 const MockAdapter = require('@bot-whatsapp/database/mock')
 
-const conversacionInicial = addKeyword('hola').addAnswer('Hola, bienvenido al chatbot del TECNM 🇲🇽')
+const conversacionInicial = addKeyword('Hola').addAnswer('Hola, bienvenido al chatbot TECNM como ayuda de prevencion hacia "Depresion y Suicidio" en el Instituto Tecnologico de Merida ubicado en el estado de Yucatán, México')
 /** (2) DEFINICION LA FUNCION PRINCIPAL */
 const main = async () => {
     const adapterDB = new MockAdapter()
